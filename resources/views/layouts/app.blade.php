9d39861b8e2914c9d59b954b0ecf0367eac73a73
abhikk <abhishekkumar.ak955@gmail.com>
2020-07-05 22:48:44 +0530

    Assigned To 5511

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,16 @@
                                 </li>
                             @endif
                         @else
+                        <a href="/home" target="_self"><button type="button" class="btn btn-outline-primary" style="margin: 5px 5px;padding:5px">Create New Job</button>
+                        </a>
+                        <a href="/assigned" target="_self"> <button type="button" class="btn btn-outline-primary"style="margin: 5px 5px;padding:5px">Leads Assigned to You</button>
+                        </a>
+                       
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
+                        
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
