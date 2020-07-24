5095be38a3e61e93bd4a72842fd37471c41320d1
abhikk <abhishekkumar.ak955@gmail.com>
2020-07-24 21:48:43 +0530

    new fix

--- resources/views/assigned_to_you_leads.blade.php
+++ resources/views/assigned_to_you_leads.blade.php
@@ -188,7 +188,7 @@
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="{{ $c->created_at }}Title">View Lead .' '.  {{$c->created_at->diffForhumans() }}</h5>
+            <h5 class="modal-title" id="{{ $c->created_at }}Title">Assigned to you {{$c->created_at->diffForhumans() }}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
