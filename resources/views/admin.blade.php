b68090e9cffe33575b670ac1db515554a5d28a20
abhikk <abhishekkumar.ak955@gmail.com>
2021-02-22 23:45:21 +0530

    datables added

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -16,6 +16,24 @@
     });
   });
 </script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+  
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
+  
+  
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+
+  
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
 </head>
 @section('form')
 	
@@ -225,9 +243,13 @@
 			</div>
 			
 		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-
-
+	<script>
+		$('#basic-datatables').DataTable({
+  dom: 'Bfrtip',
+  buttons: [
+    'copyHtml5', 'excelHtml5', 'pdfHtml5', 'csvHtml5'
+  ]
+} );
+		</script>
 
 @endsection
