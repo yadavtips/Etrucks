86752aa3a77930d94ccae152c6ac57cab3b9635f
abhikk <abhishekkumar.ak955@gmail.com>
2021-03-01 23:45:19 +0530

    delete option

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app_admin')
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 @section('form')
 
 <table class="table table-striped">
@@ -22,11 +23,29 @@
         <th scope="row">{{$user->name}}</th>
         <td>{{$user->email}}</td>
         <td>
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal{{$user->id}}">
+  Delete User
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+      
+        <div class="modal-body">
             <form action="{{ route('account.destroy',$user->id) }}" method="POST" >
                 @method('DELETE')
                 @csrf
             <button type="submit" class="btn btn-danger">Delete</button></td>
+            
             </form>
+        </div>
+      
+      </div>
+    </div>
+  </div>
+           
         <td> <form action="{{ route('account.update',$user->id) }}" method="POST" >
             @method('PUT')
             @csrf
