7f8690c86412fb598cf0e87d83330928d809e699
abhikk <abhishekkumar.ak955@gmail.com>
2021-03-16 00:56:36 +0530

    edit booking added data

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -12,14 +12,14 @@
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <title>Etrucks CRM</title>
                 <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-                <link rel="icon" href="../assets/img/icon.ico" type="image/x-icon"/>
+                <link rel="icon" href="/assets/img/icon.ico" type="image/x-icon"/>
             
                 <!-- Fonts and icons -->
-                <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+                <script src="/assets/js/plugin/webfont/webfont.min.js"></script>
                 <script>
                     WebFont.load({
                         google: {"families":["Lato:300,400,700,900"]},
-                        custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+                        custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['/assets/css/fonts.min.css']},
                         active: function() {
                             sessionStorage.fonts = true;
                         }
@@ -27,11 +27,11 @@
                 </script>
             <script src="https://kit.fontawesome.com/31d5ad4544.js" crossorigin="anonymous"></script>
                 <!-- CSS Files -->
-                <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-                <link rel="stylesheet" href="../assets/css/atlantis.min.css">
+                <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+                <link rel="stylesheet" href="/assets/css/atlantis.min.css">
             
                 <!-- CSS Just for demo purpose, don't include it in your project -->
-                <link rel="stylesheet" href="../assets/css/demo.css">
+                <link rel="stylesheet" href="/assets/css/demo.css">
                 <script src=“js/jquery.min.js”></script>
             </head>
             <body>
@@ -292,44 +292,44 @@
                 </div>
                 <!--   Core JS Files   -->
             
-                <script src="../assets/js/core/popper.min.js"></script>
-                <script src="../assets/js/core/bootstrap.min.js"></script>
+                <script src="/assets/js/core/popper.min.js"></script>
+                <script src="/assets/js/core/bootstrap.min.js"></script>
             
                 <!-- jQuery UI -->
-                <script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-                <script src="../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+                <script src="/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+                <script src="/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
             
                 <!-- jQuery Scrollbar -->
-                <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+                <script src="/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
             
             
                 <!-- Chart JS -->
-                <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
+                <script src="/assets/js/plugin/chart.js/chart.min.js"></script>
             
                 <!-- jQuery Sparkline -->
-                <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+                <script src="/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
             
                 <!-- Chart Circle -->
-                <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
+                <script src="/assets/js/plugin/chart-circle/circles.min.js"></script>
             
                 <!-- Datatables -->
-                <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
+                <script src="/assets/js/plugin/datatables/datatables.min.js"></script>
             
                 <!-- Bootstrap Notify -->
-                <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+                <script src="/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
             
                 <!-- jQuery Vector Maps -->
-                <script src="../assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
-                <script src="../assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+                <script src="/assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
+                <script src="/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
             
                 <!-- Sweet Alert -->
-                <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+                <script src="/assets/js/plugin/sweetalert/sweetalert.min.js"></script>
             
                 <!-- Atlantis JS -->
-                <script src="../assets/js/atlantis.min.js"></script>
+                <script src="/assets/js/atlantis.min.js"></script>
             
                 <!-- Atlantis DEMO methods, don't include it in your project! -->
-                <script src="../assets/js/setting-demo.js"></script>
+                <script src="/assets/js/setting-demo.js"></script>
                
                 <script>
                     Circles.create({
