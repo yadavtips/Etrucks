6deb121b02535347b5702c5129b4ef3626d44a58
Nitin Anand <yadavtips@gmail.com>
2021-05-17 12:24:37 +0530

    revised to original

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -82,7 +82,7 @@ body{
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-danger">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
